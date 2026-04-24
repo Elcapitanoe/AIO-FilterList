99b2196b253b88853bb825a2098e0111f5317bbd
Domi Adiwijaya <domy.adiwijaya@gmail.com>
2026-04-24 20:17:10 +0700

    refactor: URL formatting

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -17,7 +17,7 @@ echo "All credit belongs entirely to the original authors and the communities th
 echo "" >> "$README_FILE"
 echo "### Direct Access" >> "$README_FILE"
 echo '```text' >> "$README_FILE"
-echo "[https://hosts.domi.my.id/AIO_Filter_List.txt](https://hosts.domi.my.id/AIO_Filter_List.txt)" >> "$README_FILE"
+echo "https://hosts.domi.my.id/AIO_Filter_List.txt" >> "$README_FILE"
 echo '```' >> "$README_FILE"
 echo "" >> "$README_FILE"
 echo "### Upstream Sources" >> "$README_FILE"
@@ -32,30 +32,32 @@ sort -t '|' -k3 -f "$CONFIG_FILE" | while IFS='|' read -r filename url title; do
         continue
     fi
     
+    filename=$(echo "$filename" | xargs | tr -d '\r')
+    url=$(echo "$url" | xargs | tr -d '\r')
+    title=$(echo "$title" | xargs | tr -d '\r')
+    
     FILE_PATH="$OUTPUT_DIR/$filename"
     LAST_UPDATE="-"
     
     if [[ -f "$FILE_PATH" ]]; then
-        EXTRACTED_LINE=$(grep -m 1 -iE '^[!#].*(last modified|last updated|updated|date|version|time)[[:space:]]*[:=]' "$FILE_PATH" | tr -d '\r')
+        TARGET_LINE=$(head -n 30 "$FILE_PATH" | grep -m 1 -iE '^[!#].*(modified|updated|date|version)' | tr -d '\r')
         
-        if [[ -n "$EXTRACTED_LINE" ]]; then
-            RAW_VAL=$(echo "$EXTRACTED_LINE" | sed -E 's/^[!#][^:=]+[:=][[:space:]]*//')
-            
-            if REGEX_MATCH=$(echo "$RAW_VAL" | grep -oE '[0-9]{4}[-/.][0-9]{2}[-/.][0-9]{2}' | head -n 1); then
-                LAST_UPDATE=$(echo "$REGEX_MATCH" | tr './' '-')
-            elif REGEX_MATCH=$(echo "$RAW_VAL" | grep -oE '[0-9]{2}[-/.][0-9]{2}[-/.][0-9]{4}' | head -n 1); then
-                D=$(echo "$REGEX_MATCH" | cut -c1-2)
-                M=$(echo "$REGEX_MATCH" | cut -c4-5)
-                Y=$(echo "$REGEX_MATCH" | cut -c7-10)
+        if [[ -n "$TARGET_LINE" ]]; then
+            if DATE_MATCH=$(echo "$TARGET_LINE" | grep -oE '20[0-9]{2}[-/.][0-9]{2}[-/.][0-9]{2}' | head -n 1); then
+                LAST_UPDATE=$(echo "$DATE_MATCH" | tr './' '-')
+            elif DATE_MATCH=$(echo "$TARGET_LINE" | grep -oE '20[0-9]{2}(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])' | head -n 1); then
+                Y=$(echo "$DATE_MATCH" | cut -c1-4)
+                M=$(echo "$DATE_MATCH" | cut -c5-6)
+                D=$(echo "$DATE_MATCH" | cut -c7-8)
                 LAST_UPDATE="$Y-$M-$D"
-            elif REGEX_MATCH=$(echo "$RAW_VAL" | grep -oE '(20[0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[12][0-9]|3[01])[0-9]*' | head -n 1); then
-                Y=$(echo "$REGEX_MATCH" | cut -c1-4)
-                M=$(echo "$REGEX_MATCH" | cut -c5-6)
-                D=$(echo "$REGEX_MATCH" | cut -c7-8)
+            elif DATE_MATCH=$(echo "$TARGET_LINE" | grep -oE '[0-9]{2}[-/.][0-9]{2}[-/.]20[0-9]{2}' | head -n 1); then
+                D=$(echo "$DATE_MATCH" | cut -c1-2)
+                M=$(echo "$DATE_MATCH" | cut -c4-5)
+                Y=$(echo "$DATE_MATCH" | cut -c7-10)
                 LAST_UPDATE="$Y-$M-$D"
-            elif echo "$RAW_VAL" | grep -iqE '(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)'; then
-                CLEAN_VAL=$(echo "$RAW_VAL" | sed -E 's/ at / /gi' | tr -d '()[],')
-                PARSED=$(date -u -d "$CLEAN_VAL" +'%Y-%m-%d' 2>/dev/null)
+            elif echo "$TARGET_LINE" | grep -iqE '(jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)'; then
+                CLEAN_STR=$(echo "$TARGET_LINE" | sed -E 's/^[!#].*(modified|updated|date|version)[^a-zA-Z0-9]*//i' | sed -E 's/ at / /gi' | tr -d '()[],')
+                PARSED=$(date -u -d "$CLEAN_STR" +'%Y-%m-%d' 2>/dev/null)
                 if [[ -n "$PARSED" ]]; then
                     LAST_UPDATE="$PARSED"
                 fi
